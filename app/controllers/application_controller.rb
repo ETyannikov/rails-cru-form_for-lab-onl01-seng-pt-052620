d74571c63093be539377078c48a5e10f8fbed196
Eddy Tyannikov <etyannikov@gmail.com>
2020-10-12 02:58:45 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,8 @@
-require 'pry'
-
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  binding.pry
+  
   
 end

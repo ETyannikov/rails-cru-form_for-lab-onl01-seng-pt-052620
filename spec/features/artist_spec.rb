cbb9724c74c9fdc8ea1d255bfe636f9ba9e76985
Eddy Tyannikov <etyannikov@gmail.com>
2020-10-12 03:01:28 +0000

    Automatically backed up by Learn

--- spec/features/artist_spec.rb
+++ spec/features/artist_spec.rb
@@ -3,8 +3,8 @@ require 'pry'
 
 describe 'navigate artist pages' do
   before do
-    binding.pry
     @artist = Artist.create(name: "My Artist", bio: "My artist bio")
+    binding.pry
   end
 
   it 'shows the name on the show page in a h1 tag' do
